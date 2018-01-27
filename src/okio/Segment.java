1221333c8cfa48453cad02ba0d2767c1682e23aa
tanglie <tanglie23@163.com>
2018-01-27 23:48:59 +0800

    Segment

--- src/okio/Segment.java
+++ src/okio/Segment.java
@@ -4,5 +4,36 @@ package okio;
  * Created by pc on 2018/1/27.
  */
 public class Segment {
+
     public static final int SIZE = 8192;
+
+    final byte[] data;
+
+    /** The next byte of application data byte to read in this segment. */
+    int pos;
+
+    Segment prev;
+
+    Segment next;
+
+    Segment() {
+        this.data = new byte[SIZE];
+    }
+
+    public Segment pop() {
+        Segment result = next != this ? next : null;
+        prev.next = next;
+        next.prev = prev;
+        next = null;
+        prev = null;
+        return result;
+    }
+
+    public Segment push(Segment segment) {
+        segment.prev = this;
+        segment.next = next;
+        next.prev = segment;
+        next = segment;
+        return segment;
+    }
 }
