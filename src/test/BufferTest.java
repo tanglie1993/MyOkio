b6f61cb73e6f4ea1d3a07cbecf1f3b35abf2638a
tanglie <tanglie23@163.com>
2018-02-23 23:22:25 +0800

    indexOfWithOffset

--- src/test/BufferTest.java
+++ src/test/BufferTest.java
@@ -300,23 +300,24 @@ public final class BufferTest {
 //        assertEquals(repeat('a', 10) + repeat('b', 20), sink.readUtf8(30));
 //    }
 //
-//    @Test public void indexOfWithOffset() throws Exception {
-//        Buffer buffer = new Buffer();
-//        int halfSegment = Segment.SIZE / 2;
-//        buffer.writeUtf8(repeat('a', halfSegment));
-//        buffer.writeUtf8(repeat('b', halfSegment));
-//        buffer.writeUtf8(repeat('c', halfSegment));
-//        buffer.writeUtf8(repeat('d', halfSegment));
-//        assertEquals(0, buffer.indexOf((byte) 'a', 0));
-//        assertEquals(halfSegment - 1, buffer.indexOf((byte) 'a', halfSegment - 1));
-//        assertEquals(halfSegment, buffer.indexOf((byte) 'b', halfSegment - 1));
-//        assertEquals(halfSegment * 2, buffer.indexOf((byte) 'c', halfSegment - 1));
-//        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment - 1));
-//        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment * 2));
-//        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment * 3));
-//        assertEquals(halfSegment * 4 - 1, buffer.indexOf((byte) 'd', halfSegment * 4 - 1));
-//    }
-//
+    @Test
+    public void indexOfWithOffset() throws Exception {
+        Buffer buffer = new Buffer();
+        int halfSegment = Segment.SIZE / 2;
+        buffer.writeUtf8(repeat('a', halfSegment));
+        buffer.writeUtf8(repeat('b', halfSegment));
+        buffer.writeUtf8(repeat('c', halfSegment));
+        buffer.writeUtf8(repeat('d', halfSegment));
+        assertEquals(0, buffer.indexOf((byte) 'a', 0));
+        assertEquals(halfSegment - 1, buffer.indexOf((byte) 'a', halfSegment - 1));
+        assertEquals(halfSegment, buffer.indexOf((byte) 'b', halfSegment - 1));
+        assertEquals(halfSegment * 2, buffer.indexOf((byte) 'c', halfSegment - 1));
+        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment - 1));
+        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment * 2));
+        assertEquals(halfSegment * 3, buffer.indexOf((byte) 'd', halfSegment * 3));
+        assertEquals(halfSegment * 4 - 1, buffer.indexOf((byte) 'd', halfSegment * 4 - 1));
+    }
+
     @Test
     public void byteAt() throws Exception {
         Buffer buffer = new Buffer();
