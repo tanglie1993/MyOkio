6aec4fecab0a022893e5eab09ff1660f43457114
tanglie <tanglie23@163.com>
2018-02-23 23:22:52 +0800

    moveFewerThanRequestedBytesWithRead

--- src/test/BufferTest.java
+++ src/test/BufferTest.java
@@ -287,19 +287,20 @@ public final class BufferTest {
 //        assertEquals(repeat('a', 10) + repeat('b', 10), sink.readUtf8(20));
 //    }
 //
-//    @Test public void moveFewerThanRequestedBytesWithRead() throws Exception {
-//        Buffer sink = new Buffer();
-//        sink.writeUtf8(repeat('a', 10));
-//
-//        Buffer source = new Buffer();
-//        source.writeUtf8(repeat('b', 20));
-//
-//        assertEquals(20, source.read(sink, 25));
-//        assertEquals(30, sink.size());
-//        assertEquals(0, source.size());
-//        assertEquals(repeat('a', 10) + repeat('b', 20), sink.readUtf8(30));
-//    }
-//
+    @Test
+    public void moveFewerThanRequestedBytesWithRead() throws Exception {
+        Buffer sink = new Buffer();
+        sink.writeUtf8(repeat('a', 10));
+
+        Buffer source = new Buffer();
+        source.writeUtf8(repeat('b', 20));
+
+        assertEquals(20, source.read(sink, 25));
+        assertEquals(30, sink.size());
+        assertEquals(0, source.size());
+        assertEquals(repeat('a', 10) + repeat('b', 20), sink.readUtf8(30));
+    }
+
     @Test
     public void indexOfWithOffset() throws Exception {
         Buffer buffer = new Buffer();
