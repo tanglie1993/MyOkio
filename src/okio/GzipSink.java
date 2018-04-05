ef530751c0db191315d6592818b7316dbb9b2eb4
tanglie <tanglie23@163.com>
2018-04-05 11:29:27 +0800

    删除注释

--- src/okio/GzipSink.java
+++ src/okio/GzipSink.java
@@ -21,36 +21,14 @@ import java.util.zip.Deflater;
 
 import static java.util.zip.Deflater.DEFAULT_COMPRESSION;
 
-/**
- * A sink that uses <a href="http://www.ietf.org/rfc/rfc1952.txt">GZIP</a> to
- * compress written data to another sink.
- *
- * <h3>Sync flush</h3>
- * Aggressive flushing of this stream may result in reduced compression. Each
- * call to {@link #flush} immediately compresses all currently-buffered data;
- * this early compression may be less effective than compression performed
- * without flushing.
- *
- * <p>This is equivalent to using {@link Deflater} with the sync flush option.
- * This class does not offer any partial flush mechanism. For best performance,
- * only call {@link #flush} when application behavior requires it.
- */
 public final class GzipSink implements Sink {
-  /** Sink into which the GZIP format is written. */
+
   private final BufferedSink sink;
-
-  /** The deflater used to compress the body. */
   private final Deflater deflater;
-
-  /**
-   * The deflater sink takes care of moving data between decompressed source and
-   * compressed sink buffers.
-   */
   private final DeflaterSink deflaterSink;
 
   private boolean closed;
 
-  /** Checksum calculated for the compressed body. */
   private final CRC32 crc = new CRC32();
 
   public GzipSink(Sink sink) {
@@ -58,33 +36,33 @@ public final class GzipSink implements Sink {
     this.deflater = new Deflater(DEFAULT_COMPRESSION, true /* No wrap */);
     this.sink = Okio.buffer(sink);
     this.deflaterSink = new DeflaterSink(this.sink, deflater);
-
     writeHeader();
   }
 
-  @Override public void write(Buffer source, long byteCount) throws IOException {
-    if (byteCount < 0) throw new IllegalArgumentException("byteCount < 0: " + byteCount);
-    if (byteCount == 0) return;
-
+  @Override
+  public void write(Buffer source, long byteCount) throws IOException {
+    if (byteCount < 0) {
+      throw new IllegalArgumentException("byteCount < 0: " + byteCount);
+    }
+    if (byteCount == 0) {
+      return;
+    }
     updateCrc(source, byteCount);
     deflaterSink.write(source, byteCount);
   }
 
-  @Override public void flush() throws IOException {
+  @Override
+  public void flush() throws IOException {
     deflaterSink.flush();
   }
 
-  @Override public Timeout timeout() {
+  @Override
+  public Timeout timeout() {
     return sink.timeout();
   }
 
   @Override public void close() throws IOException {
     if (closed) return;
-
-    // This method delegates to the DeflaterSink for finishing the deflate process
-    // but keeps responsibility for releasing the deflater's resources. This is
-    // necessary because writeFooter needs to query the processed byte count which
-    // only works when the deflater is still open.
 
     Throwable thrown = null;
     try {
@@ -110,16 +88,11 @@ public final class GzipSink implements Sink {
     if (thrown != null) Util.sneakyRethrow(thrown);
   }
 
-  /**
-   * Returns the {@link Deflater}.
-   * Use it to access stats, dictionary, compression level, etc.
-   */
   public Deflater deflater() {
     return deflater;
   }
 
   private void writeHeader() {
-    // Write the Gzip header directly into the buffer for the sink to avoid handling IOException.
     Buffer buffer = this.sink.buffer();
     buffer.writeShort((short) 0x1f8b); // Two-byte Gzip ID.
     buffer.writeByte((byte) 0x08); // 8 == Deflate compression method.
@@ -134,7 +107,6 @@ public final class GzipSink implements Sink {
     sink.writeIntLe((int) deflater.getBytesRead()); // Length of original data.
   }
 
-  /** Updates the CRC with the given bytes. */
   private void updateCrc(Buffer buffer, long byteCount) {
     for (Segment head = buffer.segmentList.getFirst(); byteCount > 0; head = head.next) {
       int segmentLength = (int) Math.min(byteCount, head.rear - head.front);
--- src/okio/GzipSource.java
+++ src/okio/GzipSource.java
@@ -20,10 +20,6 @@ import java.io.IOException;
 import java.util.zip.CRC32;
 import java.util.zip.Inflater;
 
-/**
- * A source that uses <a href="http://www.ietf.org/rfc/rfc1952.txt">GZIP</a> to
- * decompress data read from another source.
- */
 public final class GzipSource implements Source {
   private static final byte FHCRC = 1;
   private static final byte FEXTRA = 2;
@@ -35,26 +31,14 @@ public final class GzipSource implements Source {
   private static final byte SECTION_TRAILER = 2;
   private static final byte SECTION_DONE = 3;
 
-  /** The current section. Always progresses forward. */
   private int section = SECTION_HEADER;
 
-  /**
-   * Our source should yield a GZIP header (which we consume directly), followed
-   * by deflated bytes (which we consume via an InflaterSource), followed by a
-   * GZIP trailer (which we also consume directly).
-   */
   private final BufferedSource source;
 
-  /** The inflater used to decompress the deflated body. */
   private final Inflater inflater;
 
-  /**
-   * The inflater source takes care of moving data between compressed source and
-   * decompressed sink buffers.
-   */
   private final InflaterSource inflaterSource;
 
-  /** Checksum used to check both the GZIP header and decompressed body. */
   private final CRC32 crc = new CRC32();
 
   public GzipSource(Source source) {
@@ -65,16 +49,18 @@ public final class GzipSource implements Source {
   }
 
   @Override public long read(Buffer sink, long byteCount) throws IOException {
-    if (byteCount < 0) throw new IllegalArgumentException("byteCount < 0: " + byteCount);
-    if (byteCount == 0) return 0;
+    if (byteCount < 0) {
+      throw new IllegalArgumentException("byteCount < 0: " + byteCount);
+    }
+    if (byteCount == 0) {
+      return 0;
+    }
 
-    // If we haven't consumed the header, we must consume it before anything else.
     if (section == SECTION_HEADER) {
       consumeHeader();
       section = SECTION_BODY;
     }
 
-    // Attempt to read at least a byte of the body. If we do, we're done.
     if (section == SECTION_BODY) {
       long offset = sink.size();
       long result = inflaterSource.read(sink, byteCount);
@@ -85,17 +71,10 @@ public final class GzipSource implements Source {
       section = SECTION_TRAILER;
     }
 
-    // The body is exhausted; time to read the trailer. We always consume the
-    // trailer before returning a -1 exhausted result; that way if you read to
-    // the end of a GzipSource you guarantee that the CRC has been checked.
     if (section == SECTION_TRAILER) {
       consumeTrailer();
       section = SECTION_DONE;
 
-      // Gzip streams self-terminate: they return -1 before their underlying
-      // source returns -1. Here we attempt to force the underlying stream to
-      // return -1 which may trigger it to release its resources. If it doesn't
-      // return -1, then our Gzip data finished prematurely!
       if (!source.exhausted()) {
         throw new IOException("gzip finished without exhausting source");
       }
@@ -105,12 +84,6 @@ public final class GzipSource implements Source {
   }
 
   private void consumeHeader() throws IOException {
-    // Read the 10-byte header. We peek at the flags byte first so we know if we
-    // need to CRC the entire header. Then we read the magic ID1ID2 sequence.
-    // We can skip everything else in the first 10 bytes.
-    // +---+---+---+---+---+---+---+---+---+---+
-    // |ID1|ID2|CM |FLG|     MTIME     |XFL|OS | (more-->)
-    // +---+---+---+---+---+---+---+---+---+---+
     source.require(10);
     byte flags = source.buffer().getByte(3);
     boolean fhcrc = ((flags >> FHCRC) & 1) == 1;
@@ -120,10 +93,6 @@ public final class GzipSource implements Source {
     checkEqual("ID1ID2", (short) 0x1f8b, id1id2);
     source.skip(8);
 
-    // Skip optional extra fields.
-    // +---+---+=================================+
-    // | XLEN  |...XLEN bytes of "extra field"...| (more-->)
-    // +---+---+=================================+
     if (((flags >> FEXTRA) & 1) == 1) {
       source.require(2);
       if (fhcrc) updateCrc(source.buffer(), 0, 2);
@@ -132,11 +101,6 @@ public final class GzipSource implements Source {
       if (fhcrc) updateCrc(source.buffer(), 0, xlen);
       source.skip(xlen);
     }
-
-    // Skip an optional 0-terminated name.
-    // +=========================================+
-    // |...original file name, zero-terminated...| (more-->)
-    // +=========================================+
     if (((flags >> FNAME) & 1) == 1) {
       long index = source.indexOf((byte) 0);
       if (index == -1) throw new EOFException();
@@ -144,10 +108,6 @@ public final class GzipSource implements Source {
       source.skip(index + 1);
     }
 
-    // Skip an optional 0-terminated comment.
-    // +===================================+
-    // |...file comment, zero-terminated...| (more-->)
-    // +===================================+
     if (((flags >> FCOMMENT) & 1) == 1) {
       long index = source.indexOf((byte) 0);
       if (index == -1) throw new EOFException();
@@ -155,10 +115,6 @@ public final class GzipSource implements Source {
       source.skip(index + 1);
     }
 
-    // Confirm the optional header CRC.
-    // +---+---+
-    // | CRC16 |
-    // +---+---+
     if (fhcrc) {
       checkEqual("FHCRC", source.readShortLe(), (short) crc.getValue());
       crc.reset();
@@ -166,10 +122,6 @@ public final class GzipSource implements Source {
   }
 
   private void consumeTrailer() throws IOException {
-    // Read the eight-byte trailer. Confirm the body's CRC and size.
-    // +---+---+---+---+---+---+---+---+
-    // |     CRC32     |     ISIZE     |
-    // +---+---+---+---+---+---+---+---+
     checkEqual("CRC", source.readIntLe(), (int) crc.getValue());
     checkEqual("ISIZE", source.readIntLe(), (int) inflater.getBytesWritten());
   }
@@ -182,15 +134,11 @@ public final class GzipSource implements Source {
     inflaterSource.close();
   }
 
-  /** Updates the CRC with the given bytes. */
   private void updateCrc(Buffer buffer, long offset, long byteCount) {
-    // Skip segments that we aren't checksumming.
     Segment s = buffer.segmentList.getFirst();
     for (; offset >= (s.rear - s.front); s = s.next) {
       offset -= (s.rear - s.front);
     }
-
-    // Checksum one segment at a time.
     for (; byteCount > 0; s = s.next) {
       int pos = (int) (s.front + offset);
       int toUpdate = (int) Math.min(s.rear - pos, byteCount);
