cb363725f5156f6a8a61f4f3d7ea36b792b6fa6a
tanglie <tanglie23@163.com>
2018-04-15 18:20:38 +0800

    调整业务逻辑

--- src/okio/SegmentPool.java
+++ src/okio/SegmentPool.java
@@ -1,11 +1,12 @@
 package okio;
 
-/**
- * Created by pc on 2018/4/15.
- */
 public class SegmentPool {
+
+    private static final int TOTAL_COUNT = 0;
+
     static Segment segment;
 
+    static int count = 0;
 
     static synchronized Segment getSegment(){
         if(segment == null){
@@ -13,15 +14,23 @@ public class SegmentPool {
         }else{
             Segment result = segment;
             segment = null;
+            result.next = null;
+            segment = segment.next;
+            count--;
             return result;
         }
     }
 
     static synchronized void recycle(Segment segment){
+        if(count >= TOTAL_COUNT){
+            return;
+        }
         segment.front = 0;
         segment.rear = 0;
         segment.next = null;
+        segment.next = SegmentPool.segment;
         segment.prev = null;
         SegmentPool.segment = segment;
+        count++;
     }
 }
